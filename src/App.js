0ddafa5337ab72616bcc7bdaeea8109fb04e29f1
unknown <muzafarovroma@list.ru>
2021-01-06 17:57:31 +0500

    Revert "error"
    This reverts commit 29347dd8f70fdb79126d5508f81fd7780b7c2515.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import Header from './Components/Header';
 import Home from './Components/Home/Home'
 import Companys from './Components/Companys/Companys';
 import News from './Components/News/News'
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Messages_Container from './Components/Messager/Messages_Container';
 
 
 function App(props) {
   return (
-    <div>
+    <BrowserRouter>
       <Header/>
     <section className="main">
-        <Route exact path="/Home" render={() => <Home />} />
-        <Route exact path="/News" render={() => <News />} />
-        <Route exact path="/Messages" render={() => <Messages_Container />} />
+        <Route exact path="/Home" render={() => <Home posts={props.state} dispatch={props.dispatch}/>} />
+        <Route exact path="/News" render={() => <News dataNews={props.state.newsPage}/>} />
+        <Route exact path="/Messages" render={() => <Messages_Container messagesState={props.state.messagesStore} dispatch={props.dispatch}/>} />
         <aside>   
         <Companys />
         </aside> 
     </section>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,24 +3,18 @@ import ReactDOM from 'react-dom';
 import './sass/main.scss';
 import store from './redux/redux-store';
 import App from './App';
-import { BrowserRouter} from 'react-router-dom';
-import StoreContext from "./StoreContext";
 
 
 
 let drawPage = (state) =>
 {
 ReactDOM.render(
-  <BrowserRouter>
-  <StoreContext.Provider value={store}>
-    <App />
-  </StoreContext.Provider>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App state={state} dispatch={store.dispatch.bind(store)}/>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 } 
-
-
 drawPage(store.getState()); 
 store.subscribe(() =>
 {
